ec3c5653f564bd8789fe2df7cda869718ca2c3d6
Mayowa Tomosori <67711110+Mayorwise001@users.noreply.github.com>
2024-11-15 03:31:07 +0100

    ok

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,11 @@ const About =()=>{
     return(
         <div className="about">
             <h2>About Me</h2>
-            <p>I am a full-stack web developer with a passion for building visually stunning and highly functional applications.My interest in web development began at the age of 14 when I created a blog that eventually grew to attract over 80,000 followers. Since then, I've been dedicated to expanding my knowledge and remain eager to learn.
-When I'm not coding I'm probably at the gym or hanging out with my cat.😸</p>
+            <p>I am a full-stack web developer with a passion for building visually stunning and highly functional applications.</p>
+                
+                <p>My interest in web development began at the age of 14 when I created a blog that eventually grew to attract over 80,000 followers. Since then, I've been dedicated to expanding my knowledge and remain eager to learn.</p>
+                
+<p>When I'm not coding I'm probably at the gym or hanging out with my cat.😸</p>
         </div>
     )
 }
